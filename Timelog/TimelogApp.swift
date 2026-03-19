f98ab7f8475b4fe9428fa424afa4d4826d7f420c
MohammedMudathir <mohdmudathir94@gmail.com>
2026-03-19 10:05:13 +0000

    Initial Commit

---
+++ Timelog/TimelogApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimelogApp.swift
+//  Timelog
+//
+//  Created by Kulukulu  on 19/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct TimelogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
